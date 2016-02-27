3608a65130905ec03b54b17f83b170ed339ba9de
peng <dianxiangan@aliyun.com>
2016-02-28 02:10:52 +0800

    init

---
+++ src/main/java/maniac/lee/common/tool/Reflections.java
@@ -0,0 +1,14 @@
+package maniac.lee.common.tool;
+
+import com.google.common.reflect.Reflection;
+
+/**
+ * Created by peng on 16/2/28.
+ */
+public class Reflections {
+
+    public static <T> T convert(Object a, Class<T> clz) {
+
+    }
+
+}
